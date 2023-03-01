b63bbc5be69573be58e443dc9eb5cabafb635fd8
John Lennard <john@yakmoo.se>
2023-03-01 18:15:22 +1300

    remove stale bits

--- pkg/api/cloud/stack/restart.go
+++ pkg/api/cloud/stack/restart.go
@@ -2,13 +2,10 @@ package stack
 
 import (
 	"context"
-
-	"github.com/sitehostnz/gosh/pkg/utils"
 )
 
 // Restart restarts a stack on the given server.
 func (s *Client) Restart(ctx context.Context, request StopStartRestartRequest) (response StartStopRestartResponse, err error) {
 	uri := "cloud/stack/restart.json"
-	if err != nil {
 	return s.stopStartRestart(ctx, request, uri)
 }
